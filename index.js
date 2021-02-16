f308d5c95849031070bf55b55267cacc29a12e5c
Ruta <rutakalytyte@RutaKalytyteMBP.lan>
2021-02-16 11:08:07 +0200

    .

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+]
+
 app.get('/', (req, res) => {
     res.send('lai lai asasas');
 });
 
 app.get('/api/courses', (req, res) => {
-   res.send([1, 2, 3]);
+   res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if (!course) res.status(404).send('The course does not exist');
+   res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    if (!req.body.name || req.body.name.length < 3) {
+        // 400 bad request
+        res.status(400).send('Name is required and should be minimum 3 characters');
+        return;
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
 });
 
 // PORT
